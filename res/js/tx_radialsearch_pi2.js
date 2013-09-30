afdd9ce650a76aadca72967f06995102c3d9e6e9
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-09-30 20:41:55 +0000

    0.0.1 +++ Initial Release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/radialsearch/trunk@80277 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/js/tx_radialsearch_pi2.js
+++ res/js/tx_radialsearch_pi2.js
@@ -21,12 +21,19 @@
           }
           , dataType  : "jsonp"
           , success: function( data ) {
-            response( $.map( data.geonames, function( item ) {
-              return {
-                label: item.name + (item.adminName1 ? ", " + item.adminName1 : "") + ", " + item.countryName,
-                value: item.name
-              }
-            }));
+            if( "geonames" in data )
+            {
+              response( $.map( data.geonames, function( item ) {
+                return {
+                  label: item.name + (item.adminName1 ? ", " + item.adminName1 : "") + ", " + item.countryName,
+                  value: item.name
+                }
+              }));
+            }
+            return {
+              label: "Fehler",
+              value: 0
+            }
           }
           , error: function( req, error ) {
             alert( "Request failed: " + error );
