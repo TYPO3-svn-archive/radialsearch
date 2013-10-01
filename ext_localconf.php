4f4c061f1a53603851ffb963a01e489576f8a0d5
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-10-01 23:02:48 +0000

    0.0.1 +++ Initial Release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/radialsearch/trunk@80423 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,10 +9,10 @@ $cached = false;
 t3lib_extMgm::addPItoST43( $_EXTKEY, 'pi1/class.tx_radialsearch_pi1.php', '_pi1', 'list_type', $cached );
 
 $cached = false;
-t3lib_extMgm::addPItoST43( $_EXTKEY, 'pi2/class.tx_radialsearch_pi1.php', '_pi2', 'list_type', $cached );
+t3lib_extMgm::addPItoST43( $_EXTKEY, 'pi1/class.tx_radialsearch_pi1.php', '_pi2', 'list_type', $cached );
 
 $cached = false;
-t3lib_extMgm::addPItoST43( $_EXTKEY, 'pi1/class.tx_radialsearch_pi3.php', '_pi3', 'list_type', $cached );
+t3lib_extMgm::addPItoST43( $_EXTKEY, 'pi3/class.tx_radialsearch_pi3.php', '_pi3', 'list_type', $cached );
 
 $TYPO3_CONF_VARS['FE']['eID_include']['tx_radialsearch_pi1'] = 'EXT:' . $_EXTKEY . '/pi1/class.tx_radialsearch_pi1_eid.php';
 
