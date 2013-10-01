5d55d651048810f529576a2b5a68f9d5599ca11e
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-10-01 00:38:29 +0000

    0.0.1 +++ Initial Release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/radialsearch/trunk@80341 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/js/tx_radialsearch_pi1.js
+++ res/js/tx_radialsearch_pi1.js
@@ -65,11 +65,11 @@
 //          "Selected: " + ui.item.label :
 //          "Nothing selected, input was " + this.value);
 //      }
-      , open: function() {
-        $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
-      }
-      , close: function() {
-        $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
-      }
+//      , open: function() {
+//        $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+//      }
+//      , close: function() {
+//        $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+//      }
     });
   });
