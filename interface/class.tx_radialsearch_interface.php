1e04235aa84f5e6eb2d8b70e07ef74719ca16f18
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-10-05 02:23:01 +0000

    0.0.1 +++ Initial release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/radialsearch/trunk@80653 735d13b6-9817-0410-8766-e36946ffe9aa

--- interface/class.tx_radialsearch_interface.php
+++ interface/class.tx_radialsearch_interface.php
@@ -140,7 +140,7 @@ AND
     $table = $this->filter->radialsearchTable;
 
     $confFilter = $this->filter->conf_view[ 'filter.' ][ $table . '.' ][ 'conf.' ][ ' filter.' ];
-$this->pObj->dev_var_dump( $table, $confFilter );
+$this->pObj->dev_var_dump( $table, $this->filter->conf_view[ 'filter.' ][ $table . '.' ][ 'conf.' ], $confFilter );
     foreach( array_keys( ( array ) $confFilter ) as $filter )
     {
         // CONTINUE : filter has an dot
