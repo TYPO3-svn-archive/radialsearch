c01426262ef9555d3bfe43844744428b9392bd23
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-10-06 21:53:46 +0000

    0.0.1 +++ Initial release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/radialsearch/trunk@80818 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_radialsearch_pi1_eid.php
+++ pi1/class.tx_radialsearch_pi1_eid.php
@@ -106,7 +106,7 @@ class tx_radialsearch_pi1_eid extends tslib_pibase {
     $return         = $json;
     if( $jsonp_callback )
     {
-      $return = "$jsonp_callback($json)";
+      $return = $jsonp_callback . '(' . $json . ')';  // Without any spaces!
     }
 
     if( $this->drs->drsEid )
